40739d72657ab24f26177623992848f4d1a1976e
Lucky0wls <maxim.klepitsa@icloud.com>
2026-08-22 17:44:52 +0700

    Add PeSTO tapered eval
    Passed STC:
    Elo   | 140.54 +- 41.88 (95%)
    nElo  | 159.97 +- 40.41 (95%)
    SPRT  | 8.0+0.08s Threads=1 Hash=1MB
    LLR   | 2.98 (-2.94, 2.94) [0.00, 10.00]
    Games | N: 284 W: 173 L: 64 D: 47
    Penta | [9, 7, 44, 30, 52]
    
    Bench: 4070503

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -8,90 +8,150 @@ int reverseSq(const int sq) {
     return sq ^ 56;
 }   
 
-const int pawnValue = 100;
-const int knightValue = 320;
-const int bishopValue = 330;
-const int rookValue = 500;
-const int queenValue = 900;
+constexpr int pawnMgValue = 82;
+constexpr int pawnEgValue = 94;
+constexpr int knightMgValue = 337;
+constexpr int knightEgValue = 281;
+constexpr int bishopMgValue = 365;
+constexpr int bishopEgValue = 297;
+constexpr int rookMgValue = 477;
+constexpr int rookEgValue = 512;
+constexpr int queenMgValue = 1025;
+constexpr int queenEgValue = 936;
 
-const int pawnTable[64] = {
+constexpr int pawnMgTable[64] = {
     0, 0, 0, 0, 0, 0, 0, 0,
-    5, 10, 10, -20, -20, 10, 10, 5,
-    5, -5, -10, 0, 0, -10, -5, 5,
-    0, 0, 0, 20, 20, 0, 0, 0,
-    5, 5, 10, 25, 25, 10, 5, 5,
-    10, 10, 20, 30, 30, 20, 10, 10,
-    50, 50, 50, 50, 50, 50, 50, 50,
+    -35, -1, -20, -23, -15, 24, 38, -22,
+    -26, -4, -4, -10, 3, 3, 33, -12,
+    -27, -2, -5, 12, 17, 6, 10, -25,
+    -14, 13, 6, 21, 23, 12, 17, -23,
+    -6, 7, 26, 31, 65, 56, 25, -20,
+    98, 134, 61, 95, 68, 126, 34, -11,
     0, 0, 0, 0, 0, 0, 0, 0
 };
 
-const int knightTable[64] = {
-    -50, -40, -30, -30, -30, -30, -40, -50,
-    -40, -20, 0, 5, 5, 0, -20, -40,
-    -30, 5, 10, 15, 15, 10, 5, -30,
-    -30, 0, 15, 20, 20, 15, 0, -30,
-    -30, 5, 15, 20, 20, 15, 5, -30,
-    -30, 0, 10, 15, 15, 10, 0, -30,
-    -40, -20, 0, 0, 0, 0, -20, -40,
-    -50, -40, -30, -30, -30, -30, -40, -50
-};
-
-const int bishopTable[64] {
-    -20, -10, -10, -10, -10, -10, -10, -20,
-    -10, 0, 0, 0, 0, 0, 0, -10,
-    -10, 0, 5, 10, 10, 5, 0, -10,
-    -10, 5, 5, 10, 10, 5, 5, -10,
-    -10, 0, 10, 10, 10, 10, 0, -10,
-    -10, 10, 10, 10, 10, 10, 10, -10,
-    -10, 5, 0, 0, 0, 0, 5, -10,
-    -20, -10, -10, -10, -10, -10, -10, -20
-};
-
-const int rookTable[64] {
-    0, 0, 0, 5, 5, 0, 0, 0,
-    -5, 0, 0, 0, 0, 0, 0, -5,
-    -5, 0, 0, 0, 0, 0, 0, -5,
-    -5, 0, 0, 0, 0, 0, 0, -5,
-    -5, 0, 0, 0, 0, 0, 0, -5,
-    -5, 0, 0, 0, 0, 0, 0, -5,
-    5, 10, 10, 10, 10, 10, 10, 5,
+constexpr int pawnEgTable[64] = {
+    0, 0, 0, 0, 0, 0, 0, 0,
+    13, 8, 8, 10, 13, 0, 2, -7,
+    4, 7, -6, 1, 0, -5, -1, -8,
+    13, 9, -3, -7, -7, -8, 3, -1,
+    32, 24, 13, 5, -2, 4, 17, 17,
+    94, 100, 85, 67, 56, 53, 82, 84,
+    178, 173, 158, 134, 147, 132, 165, 187,
     0, 0, 0, 0, 0, 0, 0, 0
 };
 
-const int queenTable[64] {
-    -20, -10, -10, -5, -5, -10, -10, -20,
-    -10, 0, 5, 0, 0, 0, 0, -10,
-    -10, 5, 5, 5, 5, 5, 0, -10,
-    0, 0, 5, 5, 5, 5, 0, -5,
-    -5, 0, 5, 5, 5, 5, 0, -5,
-    -10, 0, 5, 5, 5, 5, 0, -10,
-    -10, 0, 0, 0, 0, 0, 0, -10,
-    -20, -10, -10, -5, -5, -10, -10, -20
+constexpr int knightMgTable[64] = {
+    -105, -21, -58, -33, -17, -28, -19, -23,
+    -29, -53, -12, -3, -1, 18, -14, -19,
+    -23, -9, 12, 10, 19, 17, 25, -16,
+    -13, 4, 16, 13, 28, 19, 21, -8,
+    -9, 17, 19, 53, 37, 69, 18, 22,
+    -47, 60, 37, 65, 84, 129, 73, 44,
+    -73, -41, 72, 36, 23, 62, 7, -17,
+    -167, -89, -34, -49, 61, -97, -15, -107
 };
 
-const int kingMgTable[64] {
-    20, 30, 10, 0, 0, 10, 30, 20,
-    20, 20, 0, 0, 0, 0, 20, 20,
-    -10, -20, -20, -20, -20, -20, -20, -10,
-    -20, -30, -30, -40, -40, -30, -30, -20,
-    -30, -40, -40, -50, -50, -40, -40, -30,
-    -30, -40, -40, -50, -50, -40, -40, -30,
-    -30, -40, -40, -50, -50, -40, -40, -30,
-    -30, -40, -40, -50, -50, -40, -40, -30
+constexpr int knightEgTable[64] = {
+    -29, -51, -23, -15, -22, -18, -50, -64,
+    -42, -20, -10, -5, -2, -20, -23, -44,
+    -23, -3, -1, 15, 10, -3, -20, -22,
+    -18, -6, 16, 25, 16, 17, 4, -18,
+    -17, 3, 22, 22, 22, 11, 8, -18,
+    -24, -20, 10, 9, -1, -9, -19, -41,
+    -25, -8, -25, -2, -9, -25, -24, -52,
+    -58, -38, -13, -28, -31, -27, -63, -99
 };
 
-const int kingEgTable[64] {
-    -50, -30, -30, -30, -30, -30, -30, -50,
-    -30, -30, 0, 0, 0, 0, -30, -30,
-    -30, -10, 20, 30, 30, 20, -10, -30,
-    -30, -10, 30, 40, 40, 30, -10, -30,
-    -30, -10, 30, 40, 40, 30, -10, -30,
-    -30, -10, 20, 30, 30, 20, -10, -30,
-    -30, -20, -10, 0, 0, -10, -20, -30,
-    -50, -40, -30, -20, -20, -30, -40, -50
+constexpr int bishopMgTable[64] = {
+    -33, -3, -14, -21, -13, -12, -39, -21,
+    4, 15, 16, 0, 7, 21, 33, 1,
+    0, 15, 15, 15, 14, 27, 18, 10,
+    -6, 13, 13, 26, 34, 12, 10, 4,
+    -4, 5, 19, 50, 37, 37, 7, -2,
+    -16, 37, 43, 40, 35, 50, 37, -2,
+    -26, 16, -18, -13, 30, 59, 18, -47,
+    -29, 4, -82, -37, -25, -42, 7, -8
 };
 
-int evaluate(const Board& board) {
+constexpr int bishopEgTable[64] = {
+    -23, -9, -23, -5, -9, -16, -5, -17,
+    -14, -18, -7, -1, 4, -9, -15, -27,
+    -12, -3, 8, 10, 13, 3, -7, -15,
+    -6, 3, 13, 19, 7, 10, -3, -9,
+    -3, 9, 12, 9, 14, 10, 3, 2,
+    2, -8, 0, -1, -2, 6, 0, 4,
+    -8, -4, 7, -12, -3, -13, -4, -14,
+    -14, -21, -11, -8, -7, -9, -17, -24,
+};
+
+constexpr int rookMgTable[64] = {
+    -19, -13, 1, 17, 16, 7, -37, -26,
+    -44, -16, -20, -9, -1, 11, -6, -71,
+    -45, -25, -16, -17, 3, 0, -5, -33,
+    -36, -26, -12, -1, 9, -7, 6, -23,
+    -24, -11, 7, 26, 24, 35, -8, -20,
+    -5, 19, 26, 36, 17, 45, 61, 16,
+    27, 32, 58, 62, 80, 67, 26, 44,
+    32, 42, 32, 51, 63, 9, 31, 43
+};
+
+constexpr int rookEgTable[64] = {
+    -9, 2, 3, -1, -5, -13, 4, -20,
+    -6, -6, 0, 2, -9, -9, -11, -3,
+    -4, 0, -5, -1, -7, -12, -8, -16,
+    3, 5, 8, 4, -5, -6, -8, -11,
+    4, 3, 13, 1, 2, 1, -1, 2,
+    7, 7, 7, 5, 4, -3, -5, -3,
+    11, 13, 13, 11, -3, 3, 8, 3,
+    13, 10, 18, 15, 12, 12, 8, 5
+};
+
+constexpr int queenMgTable[64] = {
+    -1, -18, -9, 10, -15, -25, -31, -50,
+    -35, -8, 11, 2, 8, 15, -3, 1,
+    -14, 2, -11, -2, -5, 2, 14, 5,
+    -9, -26, -9, -10, -2, -4, 3, -3,
+    -27, -27, -16, -16, -1, 17, -2, 1,
+    -13, -17, 7, 8, 29, 56, 47, 57,
+    -24, -39, -5, 1, -16, 57, 28, 54,
+    -28, 0, 29, 12, 59, 44, 43, 45
+};
+
+constexpr int queenEgTable[64] = {
+    -33, -28, -22, -43, -5, -32, -20, -41,
+    -22, -23, -30, -16, -16, -23, -36, -32,
+    -16, -27, 15, 6, 9, 17, 10, 5,
+    -18, 28, 19, 47, 31, 34, 39, 23,
+    3, 22, 24, 45, 57, 40, 57, 36,
+    -20, 6, 9, 49, 47, 35, 19, 9,
+    -17, 20, 32, 41, 58, 25, 30, 0,
+    -9, 22, 22, 27, 27, 19, 10, 20
+};
+
+constexpr int kingMgTable[64] = {
+    -15, 36, 12, -54, 8, -28, 24, 14,
+    1, 7, -8, -64, -43, -16, 9, 8,
+    -14, -14, -22, -46, -44, -30, -15, -27,
+    -49, -1, -27, -39, -46, -44, -33, -51,
+    -17, -20, -12, -27, -30, -25, -14, -36,
+    -9, 24, 2, -16, -20, 6, 22, -22,
+    29, -1, -20, -7, -8, -4, -38, -29,
+    -65, 23, 16, -15, -56, -34, 2, 13
+};
+
+constexpr int kingEgTable[64] = {
+    -53, -34, -21, -11, -28, -14, -24, -43,
+    -27, -11, 4, 13, 14, 4, -5, -17,
+    -19, -3, 11, 21, 23, 16, 7, -9,
+    -18, -4, 21, 24, 27, 23, 9, -11,
+    -8, 22, 24, 27, 26, 33, 26, 3,
+    10, 17, 23, 15, 20, 45, 44, 13,
+    -12, 17, 14, 17, 17, 38, 23, 11,
+    -74, -35, -18, -18, -11, 15, 4, -17
+};
+
+int mgEval(const Board& board) {
     int score = 0;
 
     int wP = count(board.pieces[WHITE][PAWN]);
@@ -106,106 +166,171 @@ int evaluate(const Board& board) {
     int bR = count(board.pieces[BLACK][ROOK]);
     int bQ = count(board.pieces[BLACK][QUEEN]);
 
-    score += pawnValue * wP;
-    score -= pawnValue * bP;
-
-    score += knightValue * wN;
-    score -= knightValue * bN;
-
-    score += bishopValue * wB;
-    score -= bishopValue * bB;
-    
-    score += rookValue * wR;
-    score -= rookValue * bR;
-
-    score += queenValue * wQ;
-    score -= queenValue * bQ;
-
     Bitboard wP_bb = board.pieces[WHITE][PAWN];
     Bitboard wN_bb = board.pieces[WHITE][KNIGHT];
     Bitboard wB_bb = board.pieces[WHITE][BISHOP];
     Bitboard wR_bb = board.pieces[WHITE][ROOK];
     Bitboard wQ_bb = board.pieces[WHITE][QUEEN];
-    Bitboard wK_bb = board.pieces[WHITE][KING];
 
     Bitboard bP_bb = board.pieces[BLACK][PAWN];
     Bitboard bN_bb = board.pieces[BLACK][KNIGHT];
     Bitboard bB_bb = board.pieces[BLACK][BISHOP];
     Bitboard bR_bb = board.pieces[BLACK][ROOK];
     Bitboard bQ_bb = board.pieces[BLACK][QUEEN];
-    Bitboard bK_bb = board.pieces[BLACK][KING];
+
+    score += pawnMgValue * (wP - bP);
+    score += knightMgValue * (wN - bN);
+    score += bishopMgValue * (wB - bB);
+    score += rookMgValue * (wR - bR);
+    score += queenMgValue * (wQ - bQ);
+
+    score += kingMgTable[__builtin_ctzll(board.pieces[WHITE][KING])];
+    score -= kingMgTable[reverseSq(__builtin_ctzll(board.pieces[BLACK][KING]))];
     
     while (wP_bb) {
         int sq = popSq(wP_bb);
-
-        score += pawnTable[sq];
+        score += pawnMgTable[sq];
+    }
+    while (wN_bb) {
+        int sq = popSq(wN_bb);
+        score += knightMgTable[sq];
+    }
+    while (wB_bb) {
+        int sq = popSq(wB_bb);
+        score += bishopMgTable[sq];
+    }
+    while (wR_bb) {
+        int sq = popSq(wR_bb);
+        score += rookMgTable[sq];
+    }
+    while (wQ_bb) {
+        int sq = popSq(wQ_bb);
+        score += queenMgTable[sq];
     }
     while (bP_bb) {
         int sq = popSq(bP_bb);
-
-        score -= pawnTable[reverseSq(sq)];
-    }
-
-    while (wN_bb) {
-        int sq = popSq(wN_bb);
-
-        score += knightTable[sq];
+        score -= pawnMgTable[reverseSq(sq)];
     }
     while (bN_bb) {
         int sq = popSq(bN_bb);
-
-        score -= knightTable[reverseSq(sq)];
-    }
-
-    while (wB_bb) {
-        int sq = popSq(wB_bb);
-        
-        score += bishopTable[sq];
+        score -= knightMgTable[reverseSq(sq)];
     }
     while (bB_bb) {
         int sq = popSq(bB_bb);
-
-        score -= bishopTable[reverseSq(sq)];
-    }
-
-    while (wR_bb) {
-        int sq = popSq(wR_bb);
-
-        score += rookTable[sq];
+        score -= bishopMgTable[reverseSq(sq)];
     }
     while (bR_bb) {
         int sq = popSq(bR_bb);
-
-        score -= rookTable[reverseSq(sq)];
-    }
-
-    while (wQ_bb) {
-        int sq = popSq(wQ_bb);
-
-        score += queenTable[sq];
+        score -= rookMgTable[reverseSq(sq)];
     }
     while (bQ_bb) {
         int sq = popSq(bQ_bb);
-
-        score -= queenTable[reverseSq(sq)];
+        score -= queenMgTable[reverseSq(sq)];
     }
 
-    while (wK_bb) {
-        int sq = popSq(wK_bb);
+    return score;
+}
 
-        if (wQ + bQ == 0) score += kingEgTable[sq];
-        else score += kingMgTable[sq];
-    }
-    while (bK_bb) {
-        int sq = popSq(bK_bb);
+int egEval(const Board& board) {
+    int score = 0;
 
-        if (wQ + bQ == 0) score -= kingEgTable[reverseSq(sq)];
-        else score -= kingMgTable[reverseSq(sq)];
+    int wP = count(board.pieces[WHITE][PAWN]);
+    int wN = count(board.pieces[WHITE][KNIGHT]);
+    int wB = count(board.pieces[WHITE][BISHOP]);
+    int wR = count(board.pieces[WHITE][ROOK]);
+    int wQ = count(board.pieces[WHITE][QUEEN]);
+
+    int bP = count(board.pieces[BLACK][PAWN]);
+    int bN = count(board.pieces[BLACK][KNIGHT]);
+    int bB = count(board.pieces[BLACK][BISHOP]);
+    int bR = count(board.pieces[BLACK][ROOK]);
+    int bQ = count(board.pieces[BLACK][QUEEN]);
+
+    Bitboard wP_bb = board.pieces[WHITE][PAWN];
+    Bitboard wN_bb = board.pieces[WHITE][KNIGHT];
+    Bitboard wB_bb = board.pieces[WHITE][BISHOP];
+    Bitboard wR_bb = board.pieces[WHITE][ROOK];
+    Bitboard wQ_bb = board.pieces[WHITE][QUEEN];
+
+    Bitboard bP_bb = board.pieces[BLACK][PAWN];
+    Bitboard bN_bb = board.pieces[BLACK][KNIGHT];
+    Bitboard bB_bb = board.pieces[BLACK][BISHOP];
+    Bitboard bR_bb = board.pieces[BLACK][ROOK];
+    Bitboard bQ_bb = board.pieces[BLACK][QUEEN];
+
+    score += pawnEgValue * (wP - bP);
+    score += knightEgValue * (wN - bN);
+    score += bishopEgValue * (wB - bB);
+    score += rookEgValue * (wR - bR);
+    score += queenEgValue * (wQ - bQ);
+
+    score += kingEgTable[__builtin_ctzll(board.pieces[WHITE][KING])];
+    score -= kingEgTable[reverseSq(__builtin_ctzll(board.pieces[BLACK][KING]))];
+    
+    while (wP_bb) {
+        int sq = popSq(wP_bb);
+        score += pawnEgTable[sq];
+    }
+    while (wN_bb) {
+        int sq = popSq(wN_bb);
+        score += knightEgTable[sq];
+    }
+    while (wB_bb) {
+        int sq = popSq(wB_bb);
+        score += bishopEgTable[sq];
+    }
+    while (wR_bb) {
+        int sq = popSq(wR_bb);
+        score += rookEgTable[sq];
+    }
+    while (wQ_bb) {
+        int sq = popSq(wQ_bb);
+        score += queenEgTable[sq];
+    }
+    while (bP_bb) {
+        int sq = popSq(bP_bb);
+        score -= pawnEgTable[reverseSq(sq)];
+    }
+    while (bN_bb) {
+        int sq = popSq(bN_bb);
+        score -= knightEgTable[reverseSq(sq)];
+    }
+    while (bB_bb) {
+        int sq = popSq(bB_bb);
+        score -= bishopEgTable[reverseSq(sq)];
+    }
+    while (bR_bb) {
+        int sq = popSq(bR_bb);
+        score -= rookEgTable[reverseSq(sq)];
+    }
+    while (bQ_bb) {
+        int sq = popSq(bQ_bb);
+        score -= queenEgTable[reverseSq(sq)];
     }
 
-    if (board.sideToMove == WHITE) {
-        return score;
-    } else {
-        return -score;
-    }
+    return score;
+}
+
+int evaluate(const Board& board) {
+    int mgScore = mgEval(board);
+    int egScore = egEval(board);
+
+    int knights = count(board.pieces[WHITE][KNIGHT]) + count(board.pieces[BLACK][KNIGHT]);
+    int bishops = count(board.pieces[WHITE][BISHOP]) + count(board.pieces[BLACK][BISHOP]);
+    int rooks = count(board.pieces[WHITE][ROOK]) + count(board.pieces[BLACK][ROOK]);
+    int queens = count(board.pieces[WHITE][QUEEN]) + count(board.pieces[BLACK][QUEEN]);
+
+    int knight_phase = 1;
+    int bishop_phase = 1;
+    int rook_phase = 2;
+    int queen_phase = 4;
+
+    int mgPhase = (knight_phase * knights) + (bishop_phase * bishops) + (rook_phase * rooks) + (queen_phase * queens);
+    mgPhase = std::max(0, std::min(mgPhase, 24));
+    int egPhase = 24 - mgPhase;
+
+    if (board.sideToMove == WHITE)
+        return ((mgScore * mgPhase) + (egScore * egPhase)) / 24;
+    else
+        return -((mgScore * mgPhase) + (egScore * egPhase)) / 24;
 }
