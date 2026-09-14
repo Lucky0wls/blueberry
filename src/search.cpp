68a01353318f6b7f15a681f6ce935e26014db5e6
Lucky0wls <lucky0wls@users.noreply.github.com>
2026-09-14 22:51:10 +0700

    depth 6 IIR
    Bench: 1907946

--- src/search.cpp
+++ src/search.cpp
@@ -388,7 +388,7 @@ int negamax(Board& board, int depth, int alpha, int beta, int ply, searchInfo& i
         return evaluate(board);
     }
 
-    if (depth >= 7 && tt[ttIndex].bestMove == Move::NO_MOVE) {
+    if (depth >= 6 && tt[ttIndex].bestMove == Move::NO_MOVE) {
         depth--;
     }
 
